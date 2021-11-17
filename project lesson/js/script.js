3a0952023456ad6257b7904d4c0a8fa1a0cce94b
Max <grekf2@mail.ru>
2021-11-17 21:38:52 +0600

    изминил html

--- project lesson/js/script.js
+++ project lesson/js/script.js
@@ -1,75 +1,21 @@
-"use strict";
+'use strict';
 
-console.log('arr' + " - object");
-console.log(4 + +"5");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// incr++;
-// decr--;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(incr);
-console.log(decr);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(5%2);
-
-console.log(2*4 == 8);
-
-const isChecked = true,
-      isClose = false;
-      
-console.log(isChecked && isClose);
-
-
-
-
-
-
-
-// let number = 4; 
-
-// console.log(4/0);
-// console.log('string' * 9);
-
-// const person = "Max"; 
-
-// const bool = false;
-
-// const obj = {
-//     name: "Max",
-//     age: 18,
-//     isMarried: false
-// };
-
-// console.log(obj.name);
-
-// let arr = ['plum.png', 'orange.jpg', 'grapes.bmp'];
-
-// console.log(arr[1]);
-
-
-// alert('Hello bitch, I m alive');
-
-// const result = confirm('Are you here?');
-// console.log(result);
-
-// const answer = prompt("Вам есть 18?", "18");
-// console.log(answer);
-
-// const answers = [];
-
-// answers [0] = prompt('What is your name?', '');
-// answers [1] = prompt('What is your surename?', '');
-// answers [2] = prompt('How old are you?', '');
-
-// document.write(answers);
-
-// console.log(typeof(answers));
-
-// const category = 'toys';
-//  console.log(`https://someurl.com/${category}/5`);
-
-// const user = "Max";
-
-// alert(`Привет, ${user}`);
+console.log(personalMovieDB);
