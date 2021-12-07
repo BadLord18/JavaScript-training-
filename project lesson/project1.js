8a86d82461c8fa8b5aee13a390d838919071c57f
Max <grekf2@mail.ru>
2021-12-07 23:18:28 +0600

    passed 3rd practic

--- project lesson/project1.js
+++ project lesson/project1.js
@@ -52,3 +52,25 @@
 //     }
 //     console.log(i);
 // }
+
+const str = 'test';
+
+console.log(str.toUpperCase());
+console.log(str.length); 
+
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world!";
+
+console.log(logg.slice(6, 12));
+
+console.log(logg.substr(6, 11));
+
+const num = 12.6;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseFloat(test));
